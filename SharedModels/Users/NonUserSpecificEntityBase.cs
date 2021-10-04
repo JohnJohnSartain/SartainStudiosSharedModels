cdaae2765811d11d6b9b14a28067c638d4184fa2
jhnsartain3 <jhnsartain3@gmail.com>
2021-10-04 16:30:13 -0500

    fix unable to create new objects

--- SharedModels/Users/NonUserSpecificEntityBase.cs
+++ SharedModels/Users/NonUserSpecificEntityBase.cs
@@ -7,5 +7,5 @@ public abstract class NonUserSpecificEntityBase
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
+    public string? Id { get; set; }
 }
--- SharedModels/Users/UserSpecificEntityBase.cs
+++ SharedModels/Users/UserSpecificEntityBase.cs
@@ -7,7 +7,7 @@ public abstract class UserSpecificEntityBase
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
+    public string? Id { get; set; }
 
     [BsonRepresentation(BsonType.ObjectId)]
     [BsonElement("userid")]
