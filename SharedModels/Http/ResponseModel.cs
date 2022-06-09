6ccb8a6d1359104dd6d3279cea1e7cb21e3d2779
John Sartain <jhnsartain3@gmail.com>
2022-06-08 19:37:50 -0500

    add http response model

---
+++ SharedModels/Http/ResponseModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace SartainStudios.SharedModels.Http;
+
+/// <summary>
+///     Response wrapper for data returned from IAutoWrapperHttp.
+/// </summary>
+public class ResponseModel<TEntity>
+{
+    public string Message { get; set; }
+    public TEntity Result { get; set; }
+    public bool IsError { get; set; }
+    public ErrorDetails Errors { get; set; }
+    public ResponseException ResponseException { get; set; }
+    public IEnumerable<ValidationError> ValidationErrors { get; set; }
+}
+
+public class ErrorDetails
+{
+    public string Message { get; set; }
+    public string Type { get; set; }
+    public string Source { get; set; }
+    public string Raw { get; set; }
+}
+
+public class ResponseException
+{
+    public string ExceptionMessage { get; set; }
+    public IEnumerable<ValidationError> ValidationErrors { get; set; }
+}
+
+public class ValidationError
+{
+    public string Name { get; }
+    public string Reason { get; }
+
+    public ValidationError(string name, string reason)
+    {
+        Name = name != string.Empty ? name : null;
+        Reason = reason;
+    }
+}
